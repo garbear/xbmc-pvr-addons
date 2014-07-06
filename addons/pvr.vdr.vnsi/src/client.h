73e1733c9f25f9eaa24711e1c47e4e2021df8524
Garrett Brown <garbearucla@gmail.com>
2014-07-05 21:37:06 -0700

    Remove setting 'priority'. Was used if multiple XBMC clients connected to a single vdr backend

--- addons/pvr.vdr.vnsi/src/client.h
+++ addons/pvr.vdr.vnsi/src/client.h
@@ -29,7 +29,6 @@
 #define DEFAULT_PORT          34890
 #define DEFAULT_CHARCONV      false
 #define DEFAULT_HANDLE_MSG    true
-#define DEFAULT_PRIORITY      99
 #define DEFAULT_TIMEOUT       3
 #define DEFAULT_AUTOGROUPS    false
 
@@ -37,7 +36,6 @@ extern bool         m_bCreated;
 extern std::string  g_szHostname;         ///< hostname or ip-address of the server
 extern int          g_iPort;              ///< TCP port of the vnsi server
 extern int          g_iConnectTimeout;    ///< Network connection / read timeout in seconds
-extern int          g_iPriority;          ///< The Priority this client have in response to other clients
 extern bool         g_bCharsetConv;       ///< Convert VDR's incoming strings to UTF8 character set
 extern bool         g_bHandleMessages;    ///< Send VDR's OSD status messages to XBMC OSD
 extern int          g_iTimeshift;
